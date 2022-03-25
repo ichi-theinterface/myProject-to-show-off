d68329641085efeb1171233fac84afc425ca5769
Ichiro Home DT <ichi.theinterface@gmail.com>
2022-03-25 00:01:02 -0700

    updated on 03-25-2022

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,15 +5,43 @@ const main = async () => {
   console.log("Contract deployed to:", nftContract.address);
 
   // Call the function.
-  let txn = await nftContract.mintNFT("https://ipfs.io/ipfs/QmSa95s7XFZrTpeSDmPUe8pdbawbgsD38drtmPh7GrfiFj/1.json")
+  let txn = await nftContract.mintNFT("https://ipfs.io/ipfs/QmQC38hcL4NkbefVFLFP2tCFXaKVRuUQHtVWrkw1V7ip7m/1.json")
   // Wait for it to be mined.
   await txn.wait()
   console.log("Minted NFT #1")
 
-  txn = await nftContract.mintNFT("https://ipfs.io/ipfs/QmSa95s7XFZrTpeSDmPUe8pdbawbgsD38drtmPh7GrfiFj/2.json")
+  txn = await nftContract.mintNFT("https://ipfs.io/ipfs/QmQC38hcL4NkbefVFLFP2tCFXaKVRuUQHtVWrkw1V7ip7m/2.json")
   // Wait for it to be mined.
   await txn.wait()
   console.log("Minted NFT #2")
+
+  txn = await nftContract.mintNFT("https://ipfs.io/ipfs/QmQC38hcL4NkbefVFLFP2tCFXaKVRuUQHtVWrkw1V7ip7m/3.json")
+  await txn.wait()
+  console.log("Minted NFT #3")
+
+  txn = await nftContract.mintNFT("https://ipfs.io/ipfs/QmQC38hcL4NkbefVFLFP2tCFXaKVRuUQHtVWrkw1V7ip7m/4.json")
+  await txn.wait()
+  console.log("Minted NFT #4")
+
+  txn = await nftContract.mintNFT("https://ipfs.io/ipfs/QmQC38hcL4NkbefVFLFP2tCFXaKVRuUQHtVWrkw1V7ip7m/5.json")
+  await txn.wait()
+  console.log("Minted NFT #5")
+
+  txn = await nftContract.mintNFT("https://ipfs.io/ipfs/QmQC38hcL4NkbefVFLFP2tCFXaKVRuUQHtVWrkw1V7ip7m/6.json")
+  await txn.wait()
+  console.log("Minted NFT #6")
+
+  txn = await nftContract.mintNFT("https://ipfs.io/ipfs/QmQC38hcL4NkbefVFLFP2tCFXaKVRuUQHtVWrkw1V7ip7m/7.json")
+  await txn.wait()
+  console.log("Minted NFT #7")
+
+  txn = await nftContract.mintNFT("https://ipfs.io/ipfs/QmQC38hcL4NkbefVFLFP2tCFXaKVRuUQHtVWrkw1V7ip7m/8.json")
+  await txn.wait()
+  console.log("Minted NFT #8")
+
+  txn = await nftContract.mintNFT("https://ipfs.io/ipfs/QmQC38hcL4NkbefVFLFP2tCFXaKVRuUQHtVWrkw1V7ip7m/9.json")
+  await txn.wait()
+  console.log("Minted NFT #9")
 };
 
 const runMain = async () => {
