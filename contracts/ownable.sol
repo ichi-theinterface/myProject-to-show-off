3b21b19b2d6af1b3fab101377ffa5eb0bf983e0f
Ichiro Home DT <ichi.theinterface@gmail.com>
2022-04-02 00:47:03 -0700

    try to make it ownable on 04-02-2022

--- contracts/ownable.sol
+++ contracts/ownable.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
+
+contract ownable {
+    address public owner;
+
+    constructor () {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+      require(msg.sender == owner);
+      _;
+   }
+
+    function setOwner(address _newOwner) external onlyOwner {
+        require(_newOwner != address(0), "invalid address");
+        owner = _newOwner;
+    }
+
+    function onlyOwnerCanCallThisFunc() external onlyOwner {
+        // code
+    }
+
+    function anyOneCanCall() external onlyOwner{
+        // code
+    }
+}
